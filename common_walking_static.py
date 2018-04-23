6ac468e537e49e8905d6ae4d8682a8b07d6133d4
David Tsai <david@wecsam.com>
2018-04-23 17:07:52 -0400

    Renamed Walking pickle to WalkingStatic

--- common_walking_static.py
+++ common_walking_static.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 from common import file_in_this_dir
-WALKING_TIMES_PICKLE = file_in_this_dir("Walking.pickle")
+WALKING_TIMES_PICKLE = file_in_this_dir("WalkingStatic.pickle")
