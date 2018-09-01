c777d20c5e40cd548c39fb8130ce0f36769a20dd
David Tsai <david@wecsam.com>
2018-08-31 23:22:40 -0400

    Updated NYUSchedule.get_columns_indices to handle more than two nodes

--- common_nyu.py
+++ common_nyu.py
@@ -12,43 +12,68 @@ DAYS_OF_WEEK = (
     "Sunday"
 )
 
+def _deques_increasing_first(list_of_deques, greater_than=None):
+    '''
+    Yields tuples. In each tuple, a) each item is a value from a deque and
+    b) each item is greater than the previous. The first item is greater than
+    greater_than. This generator yields all combinations that satisfy these
+    conditions. It is assumed that the deques are sorted in ascending order.
+    '''
+    if list_of_deques:
+        # Get the first deque in the list of deques.
+        q = list_of_deques[0]
+        # Get the first value in the deque that is greater than greater_than.
+        # Discard all values before it.
+        if greater_than is not None:
+            try:
+                while q[0] <= greater_than:
+                    q.popleft()
+            except IndexError:
+                # This deque is empty. The generator must terminate.
+                return
+        # At this point, the first value in the deque is greater than
+        # greater_than.
+        for value in q:
+            # Construct the tuple, starting with the value from the deque.
+            head = (value,)
+            # If there are more deques, values from them will form the rest of
+            # the tuple. Otherwise, just yield the head with no tail.
+            if len(list_of_deques) > 1:
+                # Recursively call this generator on the rest of the deques.
+                for tail in _deques_increasing_first(
+                    list_of_deques[1:],
+                    value
+                ):
+                    yield head + tail
+            else:
+                yield head
+
 @attr.s
 class NYUSchedule:
     route = attr.ib(validator=attr.validators.instance_of(str))
     header_row = attr.ib(validator=attr.validators.instance_of(list))
     other_rows = attr.ib(validator=attr.validators.instance_of(list))
-    def get_columns_indices(self, from_node, to_node):
+    days_of_week = attr.ib()
+    def get_columns_indices(self, *nodes):
         '''
-        Yields pairs of indices. In each pair, the first index corresponds to a
-        value in self.header_row that is equal to from_node. In each pair, the
-        second index corresponds to a value in self.header_row that is equal to
-        to_node. In each pair, the first index is less than the second index.
+        Yields tuples of indices. In each tuple, the nth item is an index of
+        self.header_row where the value equals the nth argument (not counting
+        self). In each tuple, every item is greater than the last.
         '''
         # There is no guarantee that all values in the header row are unique.
-        # Also, there is no guarantee that from_node and to_node are different.
+        # Also, there is no guarantee that the requested nodes are different.
         # We assume that vehicles travel to the stops in the order in which
-        # they are stored in the schedule from left to right; to_node must be
-        # to the right of the from_node. Build lists of indices of from_node
-        # and to_node in the header row.
-        from_node_indices = []
-        to_node_indices = collections.deque()
-        for i, v in enumerate(self.header_row):
-            if v == from_node:
-                from_node_indices.append(i)
-            # Because there is no guarantee that from_node and to_node are
-            # different, we don't use elif here.
-            if v == to_node:
-                to_node_indices.append(i)
-        # Find all pairs of a from_node index and a to_node index where the
-        # former index is less than the latter. We can take advantage of the
-        # fact that the two lists are sorted.
-        for from_node_index in from_node_indices:
-            # Pop off any values that are less than from_node_index.
-            while to_node_indices and from_node_index >= to_node_indices[0]:
-                to_node_indices.popleft()
-            # Pair from_node_index with each of the remaining values.
-            for to_node_index in to_node_indices:
-                yield from_node_index, to_node_index
+        # they are stored in the schedule from left to right; each node must be
+        # to the right of the last.
+        nodes_occurrences = [collections.deque() for _ in nodes]
+        for index, header in enumerate(self.header_row):
+            for occurrences, node in zip(nodes_occurrences, nodes):
+                if header == node:
+                    occurrences.append(index)
+        # Find combinations of indices. Each combination contains one index of
+        # an occurrence of each requested node. We can take advantage of the
+        # fact that the lists of indices of occurrences are sorted.
+        return _deques_increasing_first(nodes_occurrences)
     def get_column_indices(self, from_node):
         '''
         Yields the indices that correspond to values in self.header_row that
